f6e9b68ef054dda7965391c1bb114d8c39f835b1
AVIKUMAR TALAVIYA <88608935+avikumart@users.noreply.github.com>
2022-11-14 16:52:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -32,11 +32,11 @@ def convert_df(df):
     # IMPORTANT: Cache the conversion to prevent computation on every rerun
     return df.to_csv().encode('utf-8')
 
-csv = convert_df(my_large_df)
+csv = convert_df(df)
 
 st.download_button(
     label="Download profile data as CSV",
     data=csv,
-    file_name='large_df.csv',
+    file_name='Gh_profile_df.csv',
     mime='text/csv',
 )
