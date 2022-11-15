1414dc0c0577f0327342aa03c12f6fb9abad1395
AVIKUMAR TALAVIYA <88608935+avikumart@users.noreply.github.com>
2022-11-15 16:40:06 +0530

    App_files added

---
+++ main.py
@@ -0,0 +1,41 @@
+import streamlit as st
+from transcribe import *
+import time
+
+# streamlit UI
+
+st.header("Transcribe Audio")
+fileObject = st.file_uploader(label = "Please upload your file" )
+
+
+if fileObject:
+    token, t_id = upload_file(fileObject)
+    result = {}
+    #polling
+    sleep_duration = 1
+    percent_complete = 0
+    progress_bar = st.progress(percent_complete)
+    st.text("Currently in queue")
+    while result.get("status") != "processing":
+        percent_complete += sleep_duration
+        time.sleep(sleep_duration)
+        progress_bar.progress(percent_complete/10)
+        result = get_text(token,t_id)
+    sleep_duration = 0.01
+    for percent in range(percent_complete,101):
+        time.sleep(sleep_duration)
+        progress_bar.progress(percent)
+
+st.balloons()
+st.header("Transcribed Text")
+st.subheader(result['text'])
+
+# further scope:
+#1) AssemblyAI lets the user specify the acoustic model and/or language model. 
+# You could use Streamlit’s selectbox to build a drop-down-like feature.
+
+#2) Add a feature that lets the user record their voice and transcribe it.
+# This might be slightly complicated since Streamlit doesn’t have any built-in components to record voice.
+# However, you can use HTML and JavaScript to build such a feature.
+
+
