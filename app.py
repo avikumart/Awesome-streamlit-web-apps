5bf56ffa4e74072840baafcc7a104db48044a683
AVIKUMAR TALAVIYA <88608935+avikumart@users.noreply.github.com>
2022-11-16 17:28:39 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,55 @@
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+import time
+import json
+import streamlit as st
+import numpy as np
+from PIL import Image
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+
+def load_data():
+    with open('data/weekly.json','r') as file:
+         weekly_keywords = json.load(file)
+    with open('data/combined.json') as file:
+         combined_keyword = json.load(file)
+    dates = [date for date in weekly_keywords]
+    return combined_keyword,weekly_keywords,dates
+
+def get_word_cloud(image,data,max_words,max_font_size):
+    if image == 'default':
+       wordcloud = WordCloud(width=700, height=400, repeat=True,   
+                   max_words=max_words, max_font_size=   
+                   max_font_size,background_color='white',
+                   ).generate_from_frequencies(data)
+    else:
+       path = f'data/image_masks/{image}.jpg'
+       mask = np.array(Image.open(path))
+       wordcloud = WordCloud(width=400, height=400, repeat=True, 
+                   max_words=max_words,max_font_size=   
+                   max_font_size,background_color='white',
+                   mask = mask).generate_from_frequencies(data)
+    return wordcloud
+
+st.title("2020 Word Clouds based on Google Keyword and Twitter Hashtag trends")
+image = st.sidebar.selectbox(label='Select Image Mask',options=
+['default','twitter','hashtag','heart'])
+combined_keyword,weekly_keywords,dates = load_data()
+
+
+st.header("Entire Year")
+wordcloud = get_word_cloud(image,combined_keyword,800,15)
+fig1 = plt.figure()
+plt.imshow(wordcloud)
+plt.axis("off")
+st.pyplot(fig1)
+
+st.header("Weekly")
+date = st.selectbox(label='Select Date',options=dates)
+keywords = weekly_keywords[date]
+wordcloud = get_word_cloud(image , keywords,200,25)
+fig2 = plt.figure()
+plt.imshow(wordcloud)
+plt.axis("off")
+st.pyplot(fig2)
---
+++ main.py
@@ -0,0 +1,84 @@
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+import time
+import json
+
+
+
+'''
+    Function to generate weekly dates
+'''
+
+
+def get_dates():
+    dates = pd.date_range('2020-01-01','2020-12-27',freq='7d')
+    dates = [d.strftime('%Y-%m-%d') for d in dates]
+    return dates
+
+
+'''
+    Function to get top 10 trending Twitter Hashtags and Google Keywords on given date
+'''
+
+
+def get_keywords(date):
+    result = {}
+    url = f'https://us.trend-calendar.com/trend/{date}.html'
+    r = requests.get(url)
+    if r.status_code != 200:
+        print(f'Failed to get data from {url}')
+    soup = BeautifulSoup(r.text, "html.parser")
+
+    '''
+        Getting trending hashtags on Twitter
+    '''
+# Twitter_trends
+    try:
+        twitter_trends = soup.find_all('div', class_='readmoretable')[0].find_all('div', class_='readmoretable_line')[0:10]
+        for idx,trend in enumerate(twitter_trends):
+            trend = trend.a.text.lstrip("#").lower()
+            result[trend] = result.get(trend, 0) + (10 - idx)
+    except Exception as e:
+        print(e)
+        print(f'Failed to get twitter Hashtags from {url}')
+    '''
+        Getting trending keywords on Google
+    '''
+# Google_trends
+    try:
+        google_trends = soup.find_all('div', class_='readmoretable')[1].find_all('div', class_='readmoretable_line')[0:10]
+        for idx,trend in enumerate(google_trends):
+            trend = trend.a.text.lstrip("#").lower()
+            result[trend] = result.get(trend, 0) + (10 - idx)
+        
+    except Exception as e:
+        print(e)
+        print(f'Failed to get Google Keywords from {url}')
+    print(f"Scraped Data for {date} successfully")
+    return result
+
+
+start = time.time()
+dates = get_dates()
+keywords = {}
+for date in dates:
+    keywords[date] = get_keywords(date)
+
+print("Dumping into file")
+with open('data/weekly.json','w') as file:
+    json.dump(keywords,file)
+duration = time.time() - start
+print(f"Scraping Completed :) took {duration} seconds")
+
+print("Collecting combined result")
+with open('data/weekly.json' , 'r') as file:
+    keywords = json.load(file)
+    combined_result = {}
+    for date , week_keyword in keywords.items():
+        for keyword in week_keyword:
+            combined_result[keyword] = combined_result.get(keyword,0) + week_keyword[keyword]
+    print("Dumping into file")
+    with open('data/combined.json','w') as file:
+        json.dump(combined_result,file)
+    print(f"Done :D Got {len(combined_result)} keywords")
