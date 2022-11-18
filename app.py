0fc3144870167fe9b50e95d3ad2eb548b51953be
AVIKUMAR TALAVIYA <88608935+avikumart@users.noreply.github.com>
2022-11-18 17:09:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ import numpy as np
 from PIL import Image
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
-from main import *
 
 def load_data():
     with open('data/weekly.json','r') as file:
